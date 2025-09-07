159e2a81c73e61b1d85c5e160a9d8650311a8c9c
Akshat24112001 <Akshatsinghrajput24@gmail.com>
2025-09-07 13:21:21 +0530

    Initialize server and connection to data base

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-console.log("Server is running");
+import express from "express";
+
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+import connectDB from "./config/db.js";
+
+// configuring environment variables
+dotenv.config();
+// connection to the database
+connectDB();
+
+//  initilizing the express server
+const app = express();
+// setting up the port on which server is running
+const PORT = process.env.PORT || 4000;
+// setting cors settings
+app.use(cors());
+// setting up cookie parser for using cookies
+app.use(cookieParser());
+// so that express can read json formatted file
+app.use(express.json());
+
+// starting the server
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
